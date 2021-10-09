3973e9ef47502a0a9abb3f9066a73ea8fa535aee
Asq Arrsl <asqarrsl@gmail.com>
2021-10-10 01:15:30 +0530

    Final Changes

--- app.js
+++ app.js
@@ -1,54 +1,50 @@
-var createError = require("http-errors");
-var express = require("express");
-var path = require("path");
-var cookieParser = require("cookie-parser");
-var logger = require("morgan");
-var cors = require("cors");
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var vehiclesRouter = require("./routes/vehicle");
-var partsRouter = require("./routes/parts");
-const vehicle_fav = require("./model/vehicle_fav");
-const parts_fav = require("./model/parts_fav");
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var cors = require('cors')
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var vehiclesRouter = require('./routes/vehicle');
+var partsRouter = require('./routes/parts');
 require("dotenv").config();
 require("./config/database").connect();
+
 
 var app = express();
 
 // view engine setup
-// app.set("views", path.join(__dirname, "views"));
-// app.set("view engine", "ejs");
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
 
-app.use(logger("dev"));
+app.use(logger('dev'));
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/", indexRouter);
-app.use("/api/users", usersRouter);
-app.use("/api/vehicles", vehiclesRouter);
-app.use("/api/parts", partsRouter);
-app.use("/api/part_fav", parts_fav);
-app.use("/api/vehicle_fav", vehicle_fav);
+app.use('/', indexRouter);
+app.use('/api/users', usersRouter);
+app.use('/api/vehicles', vehiclesRouter);
+app.use('/api/parts', partsRouter);
+
 
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
+app.use(function(req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  console.log(err.message);
-  // res.status(err.message);
   res.status(err.status || 500);
-  res.send(err);
+  res.render('error');
 });
 
 module.exports = app;
